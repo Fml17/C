36560d1cd859f529138f04489530167b6fb02fde
Fml17 <fml081701@gmail.com>
2020-10-15 15:35:41 -0500

    Operaciones
    Como hacer operaciones

--- variables/1_variables/variables2.c
+++ variables/1_variables/variables2.c
@@ -3,11 +3,11 @@
 #include <math.h>
 
 float a,b,c,d, e; //declarando variables 
-int a1, b1; //declarando variables 
+int a1, b1, c1; //declarando variables 
 int main()
 {
 	system("clear");
-	printf("Dame el primer numero\n");
+	printf("Dame el \'primer'\ numero\n");
 	scanf("%f",&a);
 	a1 = a;
 	printf("Dame el segundo numero\n");
@@ -15,29 +15,34 @@ int main()
 	b1 = b;
 
 	printf("Suma\n");
-		c=a+b; // Suma o adiciÃ³n
-		printf("\t%f + %f = %f\n",a,b,c);
+		c=a+b; // Suma o adicion
+		printf("\t%0.2f + %0.2f = %0.2f\n",a,b,c);
 	printf("Resta\n");
 		c=a-b; // Resta o diferencia
 		printf("\t%f - %f = %f\n",a,b,c);
 	printf("MultiplicaciÃ³n\n");
 		c=a*b; // MultiplicaciÃ³n o producto
-		printf("\t%f * %f = %f\n",a,b,c);
+		printf("\t%0.2f * %0.2f = %0.2f\n",a,b,c);
 	printf("DivisiÃ³n\n");
 		c=a/b; // DivisiÃ³n o cociente tipo real
 		printf("\t%f / %f = %f\n",a,b,c);
-	printf("MÃ³dulo\n");
-		c=a1%b1; // Resto o mÃ³dulo
-		printf("\tEl mÃ³dulo de %d/%d es %f\n",a1,b1,c);
+	printf("Modulo\n");
+	a1=a;
+	b1=b;
+		c1=a1%b1; // Resto o mÃ³dulo
+		printf("\tEl modulo de %d/%d es %d\n",a1,b1,c1);
 	printf("RaÃ­z cuadrada del primer numero\n");
 		c=sqrt(a); // RaÃ­z cuadrada
-		printf("\tLa RaÃ­z cuadrada de %f es %f\n",a,c);
+		printf("\tLa RaÃ­z cuadrada de %f es: %f\n",a,c);
 	printf("RaÃ­z cuadrada del segundo numero\n");
 		c=sqrt(b); // RaÃ­z cuadrada
 		printf("\tLa RaÃ­z cuadrada de %f es %f\n",b,c);
 	printf("Potencia\n");
 		c = pow(b, a); //Potencia
 	printf("\tEl nÃºmero %f elevado a la potencia %f es %f\n",b,a,c);
+	
+		
+	
 	
 	printf("Resultado del reto viral de Agosto\n");
 
